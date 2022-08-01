06dfce6fc47740cdc3ff8cceb06cfe8aadb93330
PaolaCG <13-10248@usb.ve>
2022-08-01 11:10:45 -0400

    edit bank

--- models/bank.py
+++ models/bank.py
@@ -5,8 +5,8 @@ class Bank(db.Model):
     __tablename__ = 'bank'
     id = db.Column( db.Integer, primary_key = True )
     amountCredit = db. Column( db.Float, nullable = False )
-    date = db.Column( db.Date, nullable=False, default= strftime("%Y-%m-%d", gmtime() ) )
-    time = db.Column( db.String(20), nullable=False, default= strftime("%H:%M:%S", gmtime() ) )
+    date = db.Column( db.String(10), nullable = False )
+    time = db.Column( db.String(20), nullable = False )
     description = db.Column(db.String(50), nullable = False)
     amount = db. Column( db.Float, nullable = False )
 
