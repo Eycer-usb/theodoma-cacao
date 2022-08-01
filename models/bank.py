59c91481456b218c2ecaa19e63368bb45a718b97
PaolaCG <13-10248@usb.ve>
2022-08-01 10:41:02 -0400

    add models bank

---
+++ models/bank.py
@@ -0,0 +1,18 @@
+from utils.db import db
+from time import gmtime, strftime
+
+class Bank(db.Model):
+    __tablename__ = 'bank'
+    id = db.Column( db.Integer, primary_key = True )
+    amountCredit = db. Column( db.Float, nullable = False )
+    date = db.Column( db.Date, nullable=False, default= strftime("%Y-%m-%d", gmtime() ) )
+    time = db.Column( db.String(20), nullable=False, default= strftime("%H:%M:%S", gmtime() ) )
+    description = db.Column(db.String(50), nullable = False)
+    amount = db. Column( db.Float, nullable = False )
+
+    def __init__(self, amountCredit, date, time, description, amount):
+        self.amountCredit = amountCredit
+        self.date = date
+        self.time = time
+        self.description = description
+        self.amount = amount
