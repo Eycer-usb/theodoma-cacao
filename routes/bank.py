5c6d489b105f6dd9e9013439e7cf3bb7ffb17540
PaolaCG <13-10248@usb.ve>
2022-08-01 11:07:55 -0400

    banks routes

--- routes/bank.py
+++ routes/bank.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, session
 from utils.functions import *
 from utils.db import db
+from models.bank import Bank
 from datetime import datetime
 
 bank = Blueprint('bank', __name__)
@@ -8,5 +9,13 @@ allowed_rols = ['admin', 'shopping-analyst']
 
 @bank.route('/bank')
 def index():
-        return render_template("bank.html")
+    if( not verify_permissions(session, User, allowed_rols) ):
+        return redirect(url_for('auth.index'))
+    banks = Bank.query.all()
 
+    if 'management-status' not in session:
+        return render_template('bank.html', status="",\
+            banks = banks)
+    status = session['management-status']
+    session.pop('management-status', None)
+    return render_template("bank.html", banks=banks)
